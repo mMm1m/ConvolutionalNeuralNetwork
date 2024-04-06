cc621c7770e87e2be2f014daf26bd114c05180d3
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-04-06 21:41:00 +0300

    Correct work train model v2.0

--- classification.py
+++ classification.py
@@ -1,9 +1,16 @@
 import torch
-import torchvision
-from torchvision import datasets, models, transforms
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
 import os
+from PIL import Image
+from tempfile import TemporaryDirectory
 
 # Предположим, что данные располагаются в каталогах 'train' и 'valid'
 data_dir = ('arch')
@@ -32,7 +39,7 @@ class_names = image_datasets['train'].classes
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Загрузка предварительно обученной модели ResNet-18
-model_ft = models.resnet18(pretrained=True)
+model_ft = models.resnet18(weights='IMAGENET1K_V1')
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, 2)
 
@@ -44,65 +51,92 @@ criterion = nn.CrossEntropyLoss()
 optimizer_ft = optim.SGD(model_ft.fc.parameters(), lr=0.001, momentum=0.9)
 
 # Уменьшение скорости обучения на 7 эпохе на 10x
-exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 # Обучение модели
-def train_model(model, criterion, optimizer, scheduler, num_epochs=3):
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
+    since = time.time()
 
-        for phase in ['train', 'valid']:
-            if phase == 'train':
-                model.train()
-            else:
-                model.eval()
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
 
-            running_loss = 0.0
-            running_corrects = 0
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
 
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
 
-                optimizer.zero_grad()
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'valid']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()  # Set model to evaluate mode
 
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
+                running_loss = 0.0
+                running_corrects = 0
 
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-            if phase == 'train':
-                scheduler.step()
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
 
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                # deep copy the model
+                if phase == 'valid' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+        # load best model weights
+        model.load_state_dict(torch.load(best_model_params_path))
     return model
 
+
 # Обучение и оценка модели
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=3)
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=5)
 
-# Оценка модели на валидационных данных
-correct = 0
-total = 0
-with torch.no_grad():
-    for inputs, labels in dataloaders['valid']:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        outputs = model_ft(inputs)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the validation images: %d %%' % (100 * correct / total))
+# # Оценка модели на валидационных данных
+# correct = 0
+# total = 0
+# with torch.no_grad():
+#     for inputs, labels in dataloaders['valid']:
+#         inputs = inputs.to(device)
+#         labels = labels.to(device)
+#         outputs = model_ft(inputs)
+#         _, predicted = torch.max(outputs, 1)
+#         total += labels.size(0)
+#         correct += (predicted == labels).sum().item()
+#
+# print('Accuracy of the network on the validation images: %d %%' % (100 * correct / total))
