77afe45e0492d6076711e365571225bef28fb7e2
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-05-15 20:43:27 +0300

    Refactoring directories and create multithreading web-server

---
+++ server/WebServer.h
@@ -0,0 +1,65 @@
+#ifndef IMAGE_CLASSIFICATION_WEBSERVER_H
+#define IMAGE_CLASSIFICATION_WEBSERVER_H
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+using namespace boost::asio;
+typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
+
+class ThreadPool : boost::asio::thread_pool {
+  public:
+
+   ThreadPool(size_t num_threads);
+
+   template<class F>
+   void enqueue(F&& f);
+
+   ~ThreadPool();
+
+   int getQueueSize();
+
+   int getSize();
+
+  private:
+   std::vector<std::thread> threads_;
+   std::queue<std::function<void()>> tasks_;
+   std::mutex mutex_;
+   std::condition_variable condition_;
+   bool stop_ = false;
+   int num_threads;
+};
+
+class HTTPServer {
+  public:
+   HTTPServer(io_service& io_service, short port);
+
+  private:
+   // business logic function
+   std::string getLogic(std::string& str);
+   void start_accept();
+
+   void handle_accept(socket_ptr socket, const boost::system::error_code& error);
+
+   void handle_request(socket_ptr socket);
+
+   ip::tcp::acceptor acceptor_;
+   io_service& io_service_;
+   //boost::asio::thread_pool pool;
+   ThreadPool pool;
+};
+
+#endif //IMAGE_CLASSIFICATION_WEBSERVER_H
---
+++ training/classification.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+from tempfile import TemporaryDirectory
+
+# Предположим, что данные располагаются в каталогах 'train' и 'valid'
+data_dir = ('arch')
+train_dir = os.path.join(data_dir, 'train')
+valid_dir = os.path.join(data_dir, 'valid')
+
+# Преобразования изображений для нормализации
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'valid': transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'valid']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'valid']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Загрузка предварительно обученной модели ResNet-18
+model_ft = models.resnet18(weights='IMAGENET1K_V1')
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 525)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Обучение только последнего слоя (классификатора)
+optimizer_ft = optim.SGD(model_ft.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Уменьшение скорости обучения на 7 эпохе на 10x
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+# Обучение модели
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
+    since = time.time()
+
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
+
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'valid']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()  # Set model to evaluate mode
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                # deep copy the model
+                if phase == 'valid' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+        # load best model weights
+        model.load_state_dict(torch.load(best_model_params_path))
+    return model
+
+
+# Обучение и оценка модели
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+
+
