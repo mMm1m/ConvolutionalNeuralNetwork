8f38ee577a1cb60563803d5458bb2ea5fdc2e91d
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-05-31 05:40:00 +0300

    Some fixes

--- server/ImageClassification.cpp
+++ server/ImageClassification.cpp
@@ -1,9 +1,9 @@
-#include "ImageClassificaion.h"
+#include "ImageClassification.h"
 
    ImageClassifier::ImageClassifier(const std::string& modelWeightsPath, const int& num_classes){ //: device("cuda") {
      std::string str = "class_";
      // Присваиваем имена классам
-     /*for(int i = 0; i < num_classes; ++i) class_names.push_back(str + std::to_string(num_classes));
+     for(int i = 0; i < num_classes; ++i) class_names.push_back(str + std::to_string(num_classes));
      // Загружаем предварительно обученную модель ResNet-18
      model = torch::vision::models::ResNet::resnet18();
 
@@ -21,13 +21,13 @@
      if (torch::cuda::is_available()) {
        device = torch::Device(torch::kCUDA);
        model->to(device);
-     }*/
+     }
    }
 
    std::string ImageClassifier::predict(const std::string& imagePath) {
      // Загружаем изображение с помощью OpenCV
      std::string predictedClass=imagePath;
-     /*cv::Mat image = cv::imread(imagePath);
+     cv::Mat image = cv::imread(imagePath);
      torch::Tensor tensor = vision::transforms::functional::MatToTensor(image);
 
      // Добавляем размерность пакета
@@ -39,15 +39,15 @@
      }
 
      // Получаем выход модели
-     torch::Tensor output = model->forward(tensor);*/
+     torch::Tensor output = model->forward(tensor);
 
      // Получаем индекс класса с наибольшей вероятностью
-     //torch::Tensor probabilities = torch::softmax(output, /*dim=*/1);
-     //torch::Tensor topPredicted = torch::argmax(probabilities, /*dim=*/1);
-     /*int predictedClassIndex = topPredicted.item<int>();
+     torch::Tensor probabilities = torch::softmax(output, /*dim=*/1);
+     torch::Tensor topPredicted = torch::argmax(probabilities, /*dim=*/1);
+     int predictedClassIndex = topPredicted.item<int>();
 
      // Получаем предполагаемый класс
-     predictedClass = class_names[predictedClassIndex];*/
+     predictedClass = class_names[predictedClassIndex];
 
      return predictedClass;
    }
